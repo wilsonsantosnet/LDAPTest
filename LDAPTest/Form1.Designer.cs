e8cc9bc9bbbb56488726e6be3a938dbbc629cd78
Wilson dos Santos <wilsonsantosnet@gmail.com>
2022-08-09 17:12:05 -0300

    Initial

---
+++ LDAPTest/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿
+namespace LDAPTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtgroup = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtuserName = new System.Windows.Forms.TextBox();
+            this.txtadminPassword = new System.Windows.Forms.TextBox();
+            this.txtadminUser = new System.Windows.Forms.TextBox();
+            this.txtcontainer = new System.Windows.Forms.TextBox();
+            this.txtdomainController = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.43628F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.56372F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1357, 777);
+            this.tableLayoutPanel1.TabIndex = 15;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtFilter);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtgroup);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtuserName);
+            this.panel1.Controls.Add(this.txtadminPassword);
+            this.panel1.Controls.Add(this.txtadminUser);
+            this.panel1.Controls.Add(this.txtcontainer);
+            this.panel1.Controls.Add(this.txtdomainController);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1351, 177);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(507, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Group";
+            // 
+            // txtgroup
+            // 
+            this.txtgroup.Location = new System.Drawing.Point(616, 103);
+            this.txtgroup.Name = "txtgroup";
+            this.txtgroup.Size = new System.Drawing.Size(366, 20);
+            this.txtgroup.TabIndex = 24;
+            this.txtgroup.Text = "Group01";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "UserName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "AdminPassword";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "AdminUser";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Container";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "DomainController";
+            // 
+            // txtuserName
+            // 
+            this.txtuserName.Location = new System.Drawing.Point(129, 103);
+            this.txtuserName.Name = "txtuserName";
+            this.txtuserName.Size = new System.Drawing.Size(366, 20);
+            this.txtuserName.TabIndex = 18;
+            this.txtuserName.Text = "User01";
+            // 
+            // txtadminPassword
+            // 
+            this.txtadminPassword.Location = new System.Drawing.Point(616, 64);
+            this.txtadminPassword.Name = "txtadminPassword";
+            this.txtadminPassword.PasswordChar = '*';
+            this.txtadminPassword.Size = new System.Drawing.Size(366, 20);
+            this.txtadminPassword.TabIndex = 17;
+            this.txtadminPassword.Text = "p0$e!d0n5555";
+            // 
+            // txtadminUser
+            // 
+            this.txtadminUser.Location = new System.Drawing.Point(616, 23);
+            this.txtadminUser.Name = "txtadminUser";
+            this.txtadminUser.Size = new System.Drawing.Size(366, 20);
+            this.txtadminUser.TabIndex = 16;
+            this.txtadminUser.Text = "wsantos";
+            // 
+            // txtcontainer
+            // 
+            this.txtcontainer.Location = new System.Drawing.Point(129, 61);
+            this.txtcontainer.Name = "txtcontainer";
+            this.txtcontainer.Size = new System.Drawing.Size(366, 20);
+            this.txtcontainer.TabIndex = 15;
+            this.txtcontainer.Text = "DC=wsantos,dc=com";
+            // 
+            // txtdomainController
+            // 
+            this.txtdomainController.Location = new System.Drawing.Point(129, 23);
+            this.txtdomainController.Name = "txtdomainController";
+            this.txtdomainController.Size = new System.Drawing.Size(366, 20);
+            this.txtdomainController.TabIndex = 14;
+            this.txtdomainController.Text = "SERVER-AD-WS.wsantos.com";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtInfo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 186);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1351, 478);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BackColor = System.Drawing.Color.Black;
+            this.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfo.ForeColor = System.Drawing.Color.Green;
+            this.txtInfo.Location = new System.Drawing.Point(0, 0);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(1351, 478);
+            this.txtInfo.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 670);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1351, 47);
+            this.panel3.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 28);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Verificar Usuário no Grupo V2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 28);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Verificar Usuário no Grupo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Conectar com Usuario e Senha";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(3, 723);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1351, 51);
+            this.panel4.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(173, 26);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "Conectar com Usuario e Senha";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Filter";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(129, 144);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(853, 20);
+            this.txtFilter.TabIndex = 27;
+            this.txtFilter.Text = "sAMAccountName=Usuario01";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1357, 777);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Teste LDAP";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtgroup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtuserName;
+        private System.Windows.Forms.TextBox txtadminPassword;
+        private System.Windows.Forms.TextBox txtadminUser;
+        private System.Windows.Forms.TextBox txtcontainer;
+        private System.Windows.Forms.TextBox txtdomainController;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFilter;
+    }
+}
+
---
+++ LDAPTest/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LDAPTest
+{
+    public partial class Form1 : Form
+    {
+        private Action<string> _atualizarTela;
+        private Action _trabalhoAtual;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this._atualizarTela = AtualizarTela;
+   
+        }
+    
+        private void ConectarUsuarioESenha()
+        {
+
+            string domainController = txtdomainController.Text;
+            string container = txtcontainer.Text;
+            string adminUser = txtadminUser.Text;
+            string adminPassword = txtadminPassword.Text;
+            string userName = txtuserName.Text;
+
+            try
+            {
+
+                txtInfo.ForeColor = System.Drawing.Color.Green;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Inicio ConectarUsuarioESenha", null, null);
+
+                var pc = new PrincipalContext(ContextType.Domain, domainController, container, adminUser, adminPassword);
+                var user = UserPrincipal.FindByIdentity(pc, userName);
+                if (user == null)
+                    _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Usuário {userName} não encontado", null, null);
+                else
+                    _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Usuário {userName} encontado", null, null);
+
+            }
+            catch (Exception ex)
+            {
+                txtInfo.ForeColor = System.Drawing.Color.Red;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}]-[error]- Ocorreu um erro em ConectarUsuarioESenha {ex.Message}", null, null);
+            }
+        }
+
+        private void VerificarUsuarioNoGrupos()
+        {
+
+            string domainController = txtdomainController.Text;
+            string container = txtcontainer.Text;
+            string adminUser = txtadminUser.Text;
+            string adminPassword = txtadminPassword.Text;
+            string userName = txtuserName.Text;
+            string group = txtgroup.Text;
+
+            try
+            {
+
+                txtInfo.ForeColor = System.Drawing.Color.Green;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Inicio VerificarUsuarioNoGrupos", null, null);
+
+                var pc = new PrincipalContext(ContextType.Domain, domainController);
+                using (var up = UserPrincipal.FindByIdentity(pc, userName))
+                {
+                    if (up == null)
+                    {
+                        _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - usuário {userName} não encontrado", null, null);
+                        return;
+                    }
+                        
+
+                    using (PrincipalSearchResult<Principal> result = up.GetGroups())
+                    {
+                        using (GroupPrincipal groupPrincipal = (GroupPrincipal)result.Where(g => g.SamAccountName.ToLower() == group.ToLower()).FirstOrDefault())
+                        {
+                            if (groupPrincipal == null)
+                                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - usuário {userName} não esta no grupo {group}", null, null);
+                            else
+                                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - usuário {userName} esta no grupo {group}", null, null);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                txtInfo.ForeColor = System.Drawing.Color.Red;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [error] - Ocorreu um erro em VerificarUsuarioNoGrupos {ex.ToString()}", null, null);
+            }
+        }
+
+
+        private void VerificarUsuarioNoGruposV2()
+        {
+
+            string domainController = txtdomainController.Text;
+            string container = txtcontainer.Text;
+            string adminUser = txtadminUser.Text;
+            string adminPassword = txtadminPassword.Text;
+            string userName = txtuserName.Text;
+            string group = txtgroup.Text;
+
+            try
+            {
+
+                txtInfo.ForeColor = System.Drawing.Color.Green;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Inicio VerificarUsuarioNoGruposV2", null, null);
+
+
+                var pc = new PrincipalContext(ContextType.Domain, domainController);
+                using (var up = UserPrincipal.FindByIdentity(pc, userName))
+                {
+                    if (up == null)
+                    {
+                        _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - usuário {userName} não encontrado", null, null);
+                        return;
+                    }
+
+                    using (PrincipalSearchResult<Principal> result = up.GetGroups(pc))
+                    {
+                        using (GroupPrincipal groupPrincipal = (GroupPrincipal)result.Where(g => g.SamAccountName.ToLower() == group.ToLower()).FirstOrDefault())
+                        {
+                            if (groupPrincipal == null)
+                                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - usuário {userName} não esta no grupo {group}", null, null);
+                            else
+                                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - usuário {userName} esta no grupo {group}", null, null);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                txtInfo.ForeColor = System.Drawing.Color.Red;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [error] - Ocorreu um erro em VerificarUsuarioNoGruposV2 {ex.ToString()}", null, null);
+            }
+        }
+        private  void ConectarUsuarioESenhaDI()
+        {
+            string ldapString = $"LDAP://{txtdomainController.Text}/{txtcontainer.Text}";
+            string adminUser = txtadminUser.Text;
+            string adminPass = txtadminPassword.Text;
+            string username = txtuserName.Text;
+
+            try
+            {
+                txtInfo.ForeColor = System.Drawing.Color.Green;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Inicio ConectarUsuarioESenhaDI", null, null);
+
+                DirectoryEntry de = new DirectoryEntry(ldapString, adminUser, adminPass, AuthenticationTypes.Secure);
+                DirectorySearcher deSearch = new DirectorySearcher(de) { SearchRoot = de, Filter = txtFilter.Text };
+
+                SearchResult searchResult = deSearch.FindOne();
+                if (searchResult != null)
+                    _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Usuário {username}  encontrado", null, null);
+                else
+                    _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [info] - Usuário {username} não encontrado", null, null);
+            }
+            catch (Exception ex)
+            {
+                txtInfo.ForeColor = System.Drawing.Color.Red;
+                _atualizarTela.BeginInvoke($"[{DateTime.Now}] - [error] - Ocorreu um erro em ConectarUsuarioESenhaDI {ex.ToString()}", null, null);
+            }
+        }
+
+        private void AtualizarTela(string msg)
+        {
+
+            if (txtInfo.InvokeRequired)
+            {
+                Action safeWrite = delegate { AtualizarTela(msg); };
+                txtInfo.BeginInvoke(safeWrite);
+            }
+            else
+                txtInfo.Text = txtInfo.Text + msg + System.Environment.NewLine;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this._trabalhoAtual = ConectarUsuarioESenha;
+            this._trabalhoAtual.BeginInvoke(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this._trabalhoAtual = VerificarUsuarioNoGrupos;
+            this._trabalhoAtual.BeginInvoke(null, null);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this._trabalhoAtual = VerificarUsuarioNoGruposV2;
+            this._trabalhoAtual.BeginInvoke(null, null);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this._trabalhoAtual = ConectarUsuarioESenhaDI;
+            this._trabalhoAtual.BeginInvoke(null, null);
+        }
+
+       
+    }
+}
